e6321e3643bf4d5830060e26ef636d788a3872b6
Rogerio_Margoni <margoni@gmail.com>
2019-01-23 19:04:01 -0200

    Atualização faltando maior e menor

--- src/main/java/com/target/treinamento/turma/ScopoGlobalcase.java
+++ src/main/java/com/target/treinamento/turma/ScopoGlobalcase.java
@@ -3,32 +3,143 @@ package com.target.treinamento.turma;
 import java.util.Scanner;
 
 public class ScopoGlobalcase {
-	
-	public static void main(String[] args[]) {
-		
-		ScopoGlobalcase scopoGlobalcase = new ScopoGlobalcase();
-		Integer opcao = ScopoGlobalcase.lerOpcao();
-		
-		switch(opcao) {
-		case 1: System.out.println("carrega valor baseado em um numero inteiro");
-		break;
-		case 2: System.out.println("listar o vetor");
-		break;
-		case 3: System.out.println("pares");
-		break;
-		case 4: System.out.println("impares");
-		break;
-		case 5: System.out.println("maior numero");
-		break;
-		case 6: System.out.println("menor numero");
-		break;
-		case 7: System.out.println("multiplicar o total do array por um numero double");
-		break;
-		case 8: System.out.println("SAIR");
-		break;
+
+	Integer[] meuArray;
+
+	public static void main(String[] args) {
+
+		ScopoGlobalcase sg = new ScopoGlobalcase();
+
+		int variavel = 0;
+
+		while (variavel != 8) {
+
+			Integer tamanho = ScopoGlobalcase.lerTamanho();
+			Integer multi	= ScopoGlobalcase.lerMulti();
+			Integer opcao	= ScopoGlobalcase.lerOpcao();
+
+			switch (opcao) {
+			case 1:
+				System.out.println("carrega valor baseado em um numero inteiro");
+				sg.carregaArray(tamanho);
+				variavel = opcao;
+				break;
+			case 2:
+				System.out.println("listar o vetor");
+				sg.listarArray(tamanho);
+				variavel = opcao;
+				break;
+			case 3:
+				System.out.println("pares");
+				sg.numerosPares(tamanho);
+				variavel = opcao;
+				break;
+			case 4:
+				System.out.println("impares");
+				sg.numerosImpares(tamanho);
+				variavel = opcao;
+				break;
+			case 5:
+				System.out.println("maior numero");
+				sg.numeroMaior(tamanho);
+				variavel = opcao;
+				break;
+			case 6:
+				System.out.println("menor numero");
+				sg.numeroMenor(tamanho);
+				variavel = opcao;
+				break;
+			case 7:
+				System.out.println("multiplicar o total do array por um numero double");
+				sg.multiplicador(tamanho, multi);
+				variavel = opcao;
+				break;
+			case 8:
+				System.out.println("SAIR");
+				variavel = opcao;
+				break;
+			}
 		}
 	}
 
+
+	private void carregaArray(Integer tamanho) {
+		this.meuArray = new Integer[tamanho];
+
+		for (int numero = 0; numero < tamanho; numero++) {
+
+			System.out.println("Inserir valores na posição " + numero + ":");
+			Scanner scanner = new Scanner(System.in);
+			this.meuArray[numero] = scanner.nextInt();
+		}
+
+	}
+
+	private void listarArray(Integer tamanho) {
+		for (int numero = 0; numero < tamanho; numero++) {
+				System.out.println(this.meuArray[numero]);
+			}
+	}
+	
+	
+	private void numerosPares(Integer tamanho) {
+		for (int numero = 0; numero < tamanho; numero++) {
+			if (this.meuArray[numero] %2 == 0) {
+				System.out.println(this.meuArray[numero]);
+			}
+		}
+	}
+
+
+	private void numerosImpares(Integer tamanho) {
+		for (int numero = 0; numero < tamanho; numero++) {
+			if (this.meuArray[numero] %2 == 1) {
+				System.out.println(this.meuArray[numero]);
+			}
+		}
+	}
+
+	
+	private void numeroMaior(Integer tamanho) {
+		
+		int maior = 0;
+		
+		for (int numero = 0; numero < tamanho; numero++) {
+			if (maior > this.meuArray[numero]) {
+				maior = this.meuArray[numero];
+			} else {
+				maior = this.meuArray[numero];
+			}
+		System.out.println("O maior numero é o : " + maior);
+		}
+	}
+
+	private void numeroMenor(Integer tamanho) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	private void multiplicador(Integer tamanho, Integer multi) {
+		for (int numero = 0; numero < tamanho; numero++) {
+			this.meuArray[numero] = this.meuArray[numero] * multi;
+			System.out.println("Numero multiplicado = " + this.meuArray[numero]);
+			}
+	}
+
+	
+	private static Integer lerTamanho() {
+		System.out.println("Informar o tamanho do array : ");
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextInt();
+	}
+
+	private static Integer lerMulti() {
+		System.out.println("Informar o numero multiplicador : ");
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextInt();
+	}
+	
 	private static Integer lerOpcao() {
 		System.out.println("Informar a opção desejada : ");
 		Scanner scanner = new Scanner(System.in);
